dd3eeba76e73c152b98cb8e7fd538ccbea74f5e5
Mariana Yamamoto <myamamot@corp.lifescan.com>
2022-01-19 09:54:53 -0500

    Load imagem from internet with AsyncImage

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,49 +7,26 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result(trackId: 1, trackName: "First Song", collectionName: "Collection Name")]
-
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }.task {
-            print("wait data to load")
-            await loadData()
-        }
-    }
-
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            print("wait data to load 2")
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+        //        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+        //            image.resizable()
+        //                .scaledToFit()
+        //        } placeholder: {
+        //            ProgressView()
+        //        }
+        //        .frame(width: 200, height: 200)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
             } else {
-                print("couldn't decode data")
+                ProgressView()
             }
-        } catch {
-            print("Invalid data")
         }
     }
 }
